fe4d53fc7adb9311defcc3d368f67f475eba8476
Quân <qmup@users.noreply.github.com>
2017-10-21 11:05:39 +0700

    FormProject

---
+++ CSharpProject/PetCareSystem/DTO/CarerDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CarerDTO
+    {
+        public String CarerID { get; set; }
+        public String CarerName { get; set; }
+        public String Password { get; set; }
+        public Int32 Phone { get; set; }
+        public String Email { get; set; }
+        public String Address { get; set; }
+    }
+}
